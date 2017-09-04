1c43f10ef2b11895d326dd2f7c2c001c16680adf
howiep <hawkarp@gmail.com>
2017-09-04 13:32:36 +0200

    simple example

---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello - Please tell me your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello: " + name);
+            Console.ReadKey();
+        }
+    }
+}
